429870a0e4a498ddd2c8a5f916cc5a923af86534
FedExUser <FedExUser@SQA-CRS-025>
2020-06-28 19:40:17 -0500

    aa

--- src/testingonly/Highest.java
+++ src/testingonly/Highest.java
@@ -39,51 +39,102 @@ public class Highest {
 
 	 
 	static String  homePath=System.getProperty("user.dir");
-	
+	static Connection L3Ar;
     public static void main(String[] args) throws InterruptedException {
    
- 
+    	/*
+    	try {
+    		Class.forName("oracle.jdbc.driver.OracleDriver");
+    		//eraL2Con=DriverManager.getConnection("jdbc:oracle:thin:@ldap://oid.inf.fedex.com:3060/INVADJ_SVC_INT,cn=OracleContext,dc=ute,dc=fedex,dc=com","INVADJ_APP","apppwdli");
+    		L3Ar=DriverManager.getConnection("jdbc:oracle:thin:@//scd03051-vip.ute.fedex.com:1531/SCD0305","test_readonly", "perftest");
+    		} catch (SQLException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	} catch (ClassNotFoundException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
     	
-    	DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
-	    capabilities.setCapability("InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION", true);
-	    capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
-	    capabilities.setCapability("ignoreZoomSetting", true);
-	    capabilities.setCapability("ignoreProtectedModeSettings", true);
-	    capabilities.setCapability("initialBrowserUrl","https://testsso.secure.fedex.com/L3/PRSApps");
-	    
-    	System.setProperty("webdriver.ie.driver", "C:\\Users\\FedExUser\\git\\MasterGUI\\drivers\\IEDriverServer.exe");
+    */
     	
-    	WebDriver driver =  new InternetExplorerDriver(capabilities);
+    	String[] resultArray=validateResults("5829065673110");
     	
-    	driver.get("https://testsso.secure.fedex.com/L3/PRSApps");
+    	System.out.println(resultArray[0]);
+    	System.out.println(resultArray[1]);
     	
-    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-  	  	driver.manage().window().maximize();     
-		
-	
-	//	/html/body/div/div/form/div/div[1]/fieldset/label[1]/input
-		driver.findElement(By.id("username")).sendKeys("477023");
-		driver.findElement(By.id("password")).sendKeys("477023");
-		driver.findElement(By.id("submit")).click();
-/*
-    	WebDriver driver;
     	
-    	FirefoxProfile profile = new FirefoxProfile(); 
-    	profile.setPreference("capability.policy.default.Window.QueryInterface", "allAccess");
-    	profile.setPreference("capability.policy.default.Window.frameElement.get","allAccess");
-    	profile.setAcceptUntrustedCertificates(true); profile.setAssumeUntrustedCertificateIssuer(true);
-    	DesiredCapabilities capabilities = new DesiredCapabilities(); 
-    	capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-    	System.setProperty("webdriver.gecko.driver",  homePath+"\\drivers\\geckodriver.exe");
-    	driver = new FirefoxDriver(capabilities);
     	
-	
-		driver.get("https://testsso.secure.fedex.com/L3/PRSApps");
-		driver.findElement(By.id("username")).sendKeys("477023");
-		driver.findElement(By.id("password")).sendKeys("477023");
-		driver.findElement(By.id("submit")).click();
-		*/
+    	
 	}
+    
+    public static String[] validateResults(String trk) {
+    	
+    	Boolean result=null;
+    	Connection con = null;
+    	String[] resultArray = new String[2];
+    	String level="3";
+    	try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	try {
+    	
+    		if (level.equals("2")){
+    			con=DriverManager.getConnection("jdbc:oracle:thin:@//scd03051-vip.ute.fedex.com:1531/SCD0305","appsread", "appsread");
+    	    	
+       	 }
+       	 else if (level.equals("3")){
+       		con=DriverManager.getConnection("jdbc:oracle:thin:@//scd03051-vip.ute.fedex.com:1531/SCD0305","appsread", "appsread");
+        	
+       	 	}
+    	
+    	}
+    	catch(Exception e) {
+    		
+    		System.out.println("Could Not Get ERA DB Connections");
+    	}
+    	
+
+    	PreparedStatement stmt = null;
+    	ResultSet rs = null;
+    	try {
+    		
+    		stmt=con.prepareStatement("select * from xxfdx.fdx_ar_adjustment_irdt where airbill_nbr=?");  
+			stmt.setString(1,trk);  
+			rs = stmt.executeQuery();
+    	} catch (SQLException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
+    	      
+    	   
+    		try {
+    			rs = stmt.executeQuery();
+    		} catch (SQLException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+    	       try {
+    			if (rs.next()==false){
+    			      System.out.println("Is NULL");
+    			      resultArray[0]="fail";
+    			      resultArray[1]="Not In ERA Database";
+    			}
+    			   else{
+    				  
+    	             
+  	    			      resultArray[0]="pass";
+  	    			      resultArray[1]="completed";
+    	              }   	          
+    			   
+    		} catch (SQLException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+    	 return resultArray;      
+}   
 }
 
 
