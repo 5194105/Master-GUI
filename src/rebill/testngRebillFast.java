2c6fe86332b954ddb93345810b3e279b3a9af372
5194105 <5194105@c0016390.corp.ds.fedex.com>
2020-12-04 14:47:59 +0000

    a

--- src/rebill/testngRebillFast.java
+++ src/rebill/testngRebillFast.java
@@ -1906,14 +1906,16 @@ public class testngRebillFast {
     	              if (statusDesc.equals("SUCCESS")) {
   	    			      resultArray[0]="pass";
   	    			      resultArray[1]="completed";
-    	              }
+    	              } 
+    	              
     	              else if (statusDesc==null){
-                     	 
-                     	 statusDesc="fail";
-                     	 errorDesc="In ERA DB But Error Not Set";
-                      }
-    			else
-    	              {
+                         	 
+                         	 statusDesc="fail";
+                         	 errorDesc="In ERA DB But Error Not Set";
+                          }
+    	            	  
+    	              
+    	              else {
   	    			      resultArray[0]="fail";
   	    			      resultArray[1]=errorDesc;
     	              }
